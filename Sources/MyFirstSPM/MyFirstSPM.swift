bca99574c6884d6fe3f7c2c626737be02c01ab3a
siddhant nigam <nigamsiddhant@gmail.com>
2021-02-02 14:31:28 +0530

    dependency added

--- Sources/MyFirstSPM/MyFirstSPM.swift
+++ Sources/MyFirstSPM/MyFirstSPM.swift
@@ -1,6 +1,20 @@
+import Alamofire
+import UIKit
+import Foundation
+
 public struct MyFirstSPM {
+    
     var text = "Hello, World!"
+    
      public static func getServerNames() {
         print("serverName Warmane")
+        
+    }
+    
+    public static func getApiCall() {
+        guard let url = URL(string: "http//google.com") else {return}
+        AF.request(url).responseJSON { (response) in
+            print(response)
+        }
     }
 }
