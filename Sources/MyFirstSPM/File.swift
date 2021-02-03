a22043c924eb30bc484085fa593f9228b41fe69f
siddhant nigam <nigamsiddhant@gmail.com>
2021-02-03 11:24:38 +0530

    story board check

---
+++ Sources/MyFirstSPM/File.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  
+//
+//  Created by mac  on 02/02/21.
+//
+
+import Foundation
+import UIKit
+
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("view did load")
+        self.view.backgroundColor = .green
+    }
+}
--- Sources/MyFirstSPM/MyFirstSPM.swift
+++ Sources/MyFirstSPM/MyFirstSPM.swift
@@ -2,13 +2,21 @@ import Alamofire
 import UIKit
 import Foundation
 
-public struct MyFirstSPM {
+public class MyFirstSPM {
     
     var text = "Hello, World!"
     
-     public static func getServerNames() {
-        print("serverName Warmane")
+    public static func getServerNames(vc: UIViewController?) {
+        print("serverName Warmane")        
+        let storyBoard = UIStoryboard.init(name: "MainView", bundle: Bundle.module)
+        let vcs: MainViewController = storyBoard.instantiateViewController(withIdentifier: "MainViewController") as! MainViewController
+        vc?.present(vcs, animated: true, completion: nil)
         
+    }
+    
+    
+    public static func SCImage(named name: String) -> UIImage? {
+      UIImage(named: name, in: Bundle.module, compatibleWith: nil)
     }
     
     public static func getApiCall() {
